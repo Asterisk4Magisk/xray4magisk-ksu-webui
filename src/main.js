584f3607f8cce2703551a87ee79848bcdc4d417a
Whale Choi <cj1369636717@gmail.com>
2024-10-04 19:31:39 +0800

    legacy apatch support

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import VueRouter from './router'
 import i18n from './locales/i18n'
 
+if (window.apatch !== undefined && window.apatch.ksu === undefined) {
+    window.ksu = window.apatch
+}
 createApp(App).use(i18n).use(VueRouter).mount('#app')
